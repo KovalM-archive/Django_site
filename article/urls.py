d6504315f59e0c04f1b2050b136f3d9d0dc1e99d
KovalM <m.koval@mail.by>
2015-07-09 13:01:24 +0300

    lessons 5

--- article/urls.py
+++ article/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     url(r'^1/', 'article.views.basic_one'),
     url(r'^2/', 'article.views.template_two'),
     url(r'^3/', 'article.views.template_three_simple'),
+    url(r'^articles/all/$', 'article.views.articles'),
+    url(r'^articles/get/(?P<article_id>\d+)/$', 'article.views.article'),
 ]
--- firstapp/urls.py
+++ firstapp/urls.py
@@ -8,4 +8,5 @@ admin.autodiscover()
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^basicview/', include('article.urls')),
+    url(r'^', include('article.urls')),
 ]
